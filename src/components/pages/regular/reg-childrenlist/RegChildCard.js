4a8e3d39cad6ee2abbada9182407c9e77741f035
monmonplaza <monmon.plaza@gmail.com>
2021-12-23 09:33:44 +0800

    fixing children list

--- src/components/pages/regular/reg-childrenlist/RegChildCard.js
+++ src/components/pages/regular/reg-childrenlist/RegChildCard.js
@@ -1,38 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import SpinnerCard from '../../../spinner/SpinnerCard';
-import RegModalQuickSponsor from './modal/RegModalQuickSponsor';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import SpinnerCard from "../../../spinner/SpinnerCard";
+import RegModalQuickSponsor from "./modal/RegModalQuickSponsor";
 
 const RegChildCard = () => {
+  const [isSponsor, setIsSponsor] = React.useState(false);
+  const handleQuickModal = () => {
+    setIsSponsor(!isSponsor);
+  };
 
-    const [isSponsor, setIsSponsor] = React.useState(false);
-    const handleQuickModal = () => { setIsSponsor(!isSponsor) }
+  return (
+    <>
+      <div className="child__card child">
+        <div className="child__img">
+          {/* <SpinnerCard /> */}
+          <img
+            src="https://demo.frontlinebusiness.com.ph/dev/ftc2021/ftc/images/angelica.jpg"
+            alt=""
+          />
+          <div className="child__content">
+            <h3>Jhon Luther</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
+              nulla laborum ratione doloribus assumenda quibusdam, placeat
+              explicabo illum voluptatibus saepe.
+            </p>
+            <button to="/" className="btn btn--blue" onClick={handleQuickModal}>
+              Sponsor
+            </button>
+          </div>
+        </div>
+      </div>
 
-    return (
-        <>
-            <div className="child__card child">
-                <div className="child__img">
-                    <SpinnerCard />
-                    {/* <img src="https://demo.frontlinebusiness.com.ph/dev/ftc2021/ftc/images/angelica.jpg" alt="" /> */}
-                    <div className="child__content">
-                        <div className="child__content__wrap">
-                            <h3 className="child__name">test</h3>
-                            <Link to="/child-info" >View Info</Link>
-                        </div>
-                        <button to="/" className="btn btn--blue" onClick={handleQuickModal}>Sponsor</button>
-                    </div>
-                </div>
+      {isSponsor && <RegModalQuickSponsor modal={{ handleQuickModal }} />}
+    </>
+  );
+};
 
-                <div className="child__bar">
-                    <div className="child__bar__progress"></div>
-                    <p className="child__bar__label"> <span>0%</span> - Sponsored</p>
-                </div>
-            </div>
-
-            {isSponsor && <RegModalQuickSponsor modal={{ handleQuickModal }} />}
-        </>
-    )
-}
-
-export default RegChildCard
+export default RegChildCard;
--- src/components/pages/regular/reg-childrenlist/RegChildrenlist.js
+++ src/components/pages/regular/reg-childrenlist/RegChildrenlist.js
@@ -1,91 +1,75 @@
-import React from 'react'
-import FrontRegHeader from '../../../header/frontend/FrontRegHeader'
-import SpinnerWindow from '../../../spinner/SpinnerWindow'
-import RegBanner from '../RegBanner'
-import RegFooter from '../RegFooter'
-import RegChildCard from './RegChildCard'
-import RegChildNodata from './RegChildNodata'
-import RegCallToAction from '../RegCallToAction'
-import Maintenance404 from '../../404/Maintenance'
-import SpinnerButton from '../../../spinner/SpinnerButton'
+import React from "react";
+import FrontRegHeader from "../../../header/frontend/FrontRegHeader";
+import SpinnerWindow from "../../../spinner/SpinnerWindow";
+import RegBanner from "../RegBanner";
+import RegFooter from "../RegFooter";
+import RegChildCard from "./RegChildCard";
+import RegChildNodata from "./RegChildNodata";
+import RegCallToAction from "../RegCallToAction";
+import Maintenance404 from "../../404/Maintenance";
+import SpinnerButton from "../../../spinner/SpinnerButton";
 
 const RegChildrenlist = () => {
-    const [isShow, setShow] = React.useState(1);
-    const handleShowResident = () => setShow(1);
-    const handleShowNonResident = () => setShow(2)
+  return (
+    <>
+      <FrontRegHeader />
+      {/* <RegBanner title={"Children List"} /> */}
+      {/* <Maintenance404 /> */}
 
+      <div className="childbanner">
+        <div className="childbanner__wrapper">
+          <div className="childbanner__content">
+            <div>
+              <h2>Give where it's most needed</h2>
+              <p>
+                Give unrestricted gifts that allow Frontline to direct these
+                funds where they are most needed. Your giving allows us to
+                continue to actively partner with needs of people in the
+                communities we serve.
+              </p>
+              <button className="btn btn--blue">DONATE NOW</button>
+            </div>
+          </div>
+          <div className="childbanner__img">
+            <img src="./images/services-bg.jpg" alt="Fca Children" />
+          </div>
+        </div>
+      </div>
 
-
-    return (
-        <>
-            <FrontRegHeader />
-            <RegBanner title={'Children List'} />
-            <Maintenance404 />
-            <div className="childlist">
-                <div className="container">
-
-                    <div className="childlist__nav">
-                        <ul>
-                            <li><button
-                                onClick={handleShowResident}
-                                className={isShow === 1 ? "active" : ""}
-                            >Resident
-                            </button>
-                            </li>
-                            <li>
-                                <button
-                                    onClick={handleShowNonResident}
-                                    className={isShow === 2 ? "active" : ""}
-                                >
-                                    Non-Resident
-                                </button>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="childlist__body">
-                        {/* <RegChildNodata />
+      <div className="childlist">
+        <div className="container">
+          <div className="childlist__body">
+            {/* <RegChildNodata />
                         <SpinnerWindow /> */}
 
-                        <div className={isShow === 1 ? "childlist__block active" : "childlist__block"}>
-                            <div className="childlist__wrapper">
-                                <RegChildCard />
-                                <RegChildCard />
-                                <RegChildCard />
-                                <RegChildCard />
-                                <RegChildCard />
-                                <RegChildCard />
-                                <RegChildCard />
-                                <RegChildCard />
-
-                            </div>
-                        </div>
-
-                        <div className={isShow === 2 ? "childlist__block active" : "childlist__block"}>
-                            <div className="childlist__wrapper">
-                                <RegChildCard />
-                                <RegChildCard />
-                                <RegChildCard />
-                                <RegChildCard />
-
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="pagination no--border children--list">
-                    <div className="pagination__wrapper">
-                        <button className="btn btn--blue">Show More</button>
-                        <div className="load__text show"><SpinnerButton /> <span>Loading...</span></div>
-                    </div>
-                </div>
+            <div className="childlist__block active">
+              <div className="childlist__wrapper">
+                <RegChildCard />
+                <RegChildCard />
+                <RegChildCard />
+                <RegChildCard />
+                <RegChildCard />
+                <RegChildCard />
+                <RegChildCard />
+                <RegChildCard />
+              </div>
             </div>
-            <RegCallToAction />
-            <RegFooter />
+          </div>
+        </div>
 
+        <div className="pagination no--border children--list">
+          <div className="pagination__wrapper">
+            <button className="btn btn--blue">Show More</button>
+            <div className="load__text show">
+              <SpinnerButton /> <span>Loading...</span>
+            </div>
+          </div>
+        </div>
+      </div>
 
-        </>
-    )
-}
+      <RegFooter />
+    </>
+  );
+};
 
-export default RegChildrenlist
+export default RegChildrenlist;
