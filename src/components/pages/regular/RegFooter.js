9048695b5c86a6a15ebee98c1ce1acf5ac70512e
monmonplaza <monmon.plaza@gmail.com>
2021-12-23 15:53:28 +0800

    123

--- src/components/pages/regular/RegFooter.js
+++ src/components/pages/regular/RegFooter.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaPhoneAlt } from "react-icons/fa";
+import { ImMobile } from "react-icons/im";
+import { BsEnvelopeFill, BsFillHouseFill } from "react-icons/bs";
 
 const RegFooter = () => {
   return (
@@ -7,41 +10,78 @@ const RegFooter = () => {
       <footer className="footer">
         <div className="container">
           <div className="footer__wrapper">
-            <img src="./images/logo-white.png" alt="fca logo white" />
+            <div className="footer__item">
+              <img src="./images/logo-white.png" alt="fca logo white" />
+            </div>
+            <div className="footer__item">
+              <h3>Contact Details</h3>
+              <ul>
+                <li>
+                  <FaPhoneAlt /> 049 521 0732
+                </li>
+                <li>
+                  <ImMobile /> 0908 202 0749
+                </li>
+                <li>
+                  <ImMobile /> 0926 363 9722
+                </li>
+                <li>
+                  <BsEnvelopeFill /> info@fca.edu.ph
+                </li>
+                <li>
+                  <BsFillHouseFill /> Flora Drive, Brgy. San Ignacio, San Pablo
+                  City, Laguna, 4000
+                </li>
+              </ul>
+            </div>
 
-            <h3>Quick Links</h3>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/">About</Link>
-              </li>
-              <li>
-                <Link to="/">Admission</Link>
-              </li>
-              <li>
-                <Link to="/">Services</Link>
-              </li>
-              <li>
-                <Link to="/">Media</Link>
-              </li>
-              <li>
-                <Link to="/">Contact</Link>
-              </li>
-              <li>
-                <Link to="/">Donate</Link>
-              </li>
-            </ul>
+            <div className="footer__item">
+              <h3>Quick Links</h3>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/">About</Link>
+                </li>
+                <li>
+                  <Link to="/">Admission</Link>
+                </li>
+                <li>
+                  <Link to="/">Services</Link>
+                </li>
+                <li>
+                  <Link to="/">Media</Link>
+                </li>
+                <li>
+                  <Link to="/">Contact</Link>
+                </li>
+                <li>
+                  <Link to="/">Donate</Link>
+                </li>
+              </ul>
+            </div>
+
+            <div className="footer__item">
+              <h3>SCHOOL PORTAL</h3>
+              <ul>
+                <li>
+                  <a href="#">Facebook Page</a>
+                </li>
+                <li>
+                  <a href="#">FCA SMIS</a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </footer>
-      <div className="copyright">
+      {/* <div className="copyright">
         <p>
           &copy; 2021 Frontline Christian Academy <br /> Visit
           <Link to="/"> Philippine Frontline Ministries</Link>
         </p>
-      </div>
+      </div> */}
     </>
   );
 };
